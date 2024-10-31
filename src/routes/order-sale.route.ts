31b25425758d225373eb79562b766d29008ad6a6
hieutt03 <hieuttjob@gmail.com>
2024-10-31 14:44:15 +0700

    update permission

--- src/routes/order-sale.route.ts
+++ src/routes/order-sale.route.ts
@@ -12,7 +12,7 @@ const orderSaleRoute: Router = Router();
 orderSaleRoute.get("/", getAllOrderSale);
 orderSaleRoute.post("/", authMiddleware,createOrderSale);
 orderSaleRoute.get("/buyer", authMiddleware, getOrdersByUserId);
-orderSaleRoute.get("/:orderSaleId", authMiddleware, isStaffOrManager, getOrderDetailByOrderId);
+orderSaleRoute.get("/:orderSaleId", authMiddleware, getOrderDetailByOrderId);
 orderSaleRoute.put("/updateOrder/:orderSaleId", authMiddleware, updateTotalOrderSaleStatus);
 orderSaleRoute.put("/updateOrderDetail/:orderSaleDetailId", authMiddleware, updateStatusOrderDetail);
 export default orderSaleRoute;
