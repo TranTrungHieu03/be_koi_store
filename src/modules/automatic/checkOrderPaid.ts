f56bcd9db80a1e8d6f1e476e5ffd6678c4c9f295
hieutt03 <hieuttjob@gmail.com>
2024-11-02 00:51:16 +0700

    update get order

--- src/modules/automatic/checkOrderPaid.ts
+++ src/modules/automatic/checkOrderPaid.ts
@@ -1,7 +1,10 @@
 import cron from 'node-cron';
 import {OrderSaleService} from "../order-sale/order-sale.service";
 
+export const orderCron = () => {
+    console.log('Đang kiểm tra và hủy đơn hàng chưa thanh toán...');
+    cron.schedule('*/10 * * * *', async () => {
+        await OrderSaleService.cancelOrder()
+    });
 
-cron.schedule('*/10 * * * *', async () => {
-    await OrderSaleService.cancelOrder()
-});
+}
