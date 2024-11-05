5a87fd1fd2a5a3f384c6a7956849deb9c61edd9e
hieutt03 <hieuttjob@gmail.com>
2024-11-06 01:37:44 +0700

    update order route

--- src/routes/order-esign.route.ts
+++ src/routes/order-esign.route.ts
@@ -11,15 +11,20 @@ import {authMiddleware, isStaffOrManager} from "../middleware/auth.middleware";
 
 const orderEsignRoute: Router = Router();
 
-orderEsignRoute.put("/check-price-new", checkPriceForNewOrder);
-orderEsignRoute.post("/", createOrderEsign);
-orderEsignRoute.get("/", authMiddleware, getAllOrderEsign)
-orderEsignRoute.get("/buyer", authMiddleware, getAllOrderEsignByBuyer)
-orderEsignRoute.get("/:orderEsignId", getOrderEsgin)
-orderEsignRoute.put("/:orderEsignId", updateEsginDetailByStaff);
-orderEsignRoute.put("/confirm/:orderEsignId", authMiddleware, confirmOrderEsginByCustomer);
-orderEsignRoute.put("/update/:orderEsignId", authMiddleware, updateStatusAfterShipping)
+orderEsignRoute.post("/", createOrderEsign); // Tạo đơn e-sign mới
+orderEsignRoute.get("/", authMiddleware, getAllOrderEsign); // Lấy tất cả đơn e-sign
+orderEsignRoute.get("/buyer", authMiddleware, getAllOrderEsignByBuyer); // Lấy tất cả đơn e-sign của người mua
+orderEsignRoute.get("/:orderEsignId", getOrderEsgin); // Lấy thông tin đơn e-sign theo ID
+
+// Kiểm tra giá, xác nhận, cập nhật trạng thái
+orderEsignRoute.put("/check-price-new", checkPriceForNewOrder); // Kiểm tra giá mới
+orderEsignRoute.put("/:orderEsignId", updateEsginDetailByStaff); // Nhân viên cập nhật thông tin đơn e-sign
+orderEsignRoute.put("/confirm/:orderEsignId", authMiddleware, confirmOrderEsginByCustomer); // Khách hàng xác nhận đơn
+orderEsignRoute.put("/update/:orderEsignId", authMiddleware, updateStatusAfterShipping); // Cập nhật trạng thái sau khi giao hàng
+
+// Xác nhận sau khi mua
 orderEsignRoute.post("/afterBuy/:orderId", authMiddleware, confirmCareAfterBuy);
+
 
 
 
